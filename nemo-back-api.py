d3c338ed6c69ad61301f8c89f91d99d115a9e2df
philippart-s <wildagsx@gmail.com>
2022-12-28 16:28:33 +0000

    doc: 📝 add some comments

--- nemo-back-api.py
+++ nemo-back-api.py
@@ -15,16 +15,17 @@ import soundfile as sf
 
 # Transform the sound into a csv file.
 # The sound is read from the global variable sended_sound
-# Return: The builded dataset with the given sound
+# Return: The builded dataframe with the given sound
 def build_dataframe():  
-  # define the column names
+  # define the column names 🔤
   headers = 'filename length chroma_stft_mean chroma_stft_var rms_mean rms_var spectral_centroid_mean spectral_centroid_var spectral_bandwidth_mean \
       spectral_bandwidth_var rolloff_mean rolloff_var zero_crossing_rate_mean zero_crossing_rate_var harmony_mean harmony_var perceptr_mean perceptr_var tempo mfcc1_mean mfcc1_var mfcc2_mean \
       mfcc2_var mfcc3_mean mfcc3_var mfcc4_mean mfcc4_var\n'
 
-  # calculate the value of the librosa parameters
+  # Read the sended sound 🎼
   audio_time_series, sampling_rate_ori = sf.read(io.BytesIO(sended_sound))
-  # Resampling to use the same samplingrate that the model use for training
+  
+  # Librosa magic 🪄
   sampling_rate = 22050
   audio_time_series = librosa.resample(audio_time_series, sampling_rate_ori, sampling_rate)
   chroma_stft = librosa.feature.chroma_stft(y = audio_time_series, sr = sampling_rate)
@@ -34,12 +35,14 @@ def build_dataframe():
   rolloff = librosa.feature.spectral_rolloff(y = audio_time_series, sr = sampling_rate)
   zcr = librosa.feature.zero_crossing_rate(audio_time_series)
   mfcc = librosa.feature.mfcc(y = audio_time_series, sr = sampling_rate)
+
+  # Build the in memory CSV 📃 
   to_append = f'uploaded-sound {np.mean(chroma_stft)} {np.mean(rmse)} {np.mean(spec_cent)} {np.mean(spec_bw)} {np.mean(rolloff)} {np.mean(zcr)}'
   for e in mfcc:
       to_append += f' {np.mean(e)}'
-  
-  # Return the dataset
   in_memory_csv = headers + to_append
+
+  # Return the dataframe 🗄
   dataframe = pandas.read_csv(io.StringIO(in_memory_csv), sep=" ")
 
   return dataframe
@@ -53,32 +56,31 @@ GraySeal\nGrayWhale\nHarborPorpoise\nHarbourSeal\nHarpSeal\nHeaviside'sDolphin\n
 LeopardSeal\nLong_FinnedPilotWhale\nLongBeaked(Pacific)CommonDolphin\nMelonHeadedWhale\nMinkeWhale\nNarwhal\nNewZealandFurSeal\nNorthernRightWhale\n\
 PantropicalSpottedDolphin\nRibbonSeal\nRingedSeal\nRossSeal\nRough_ToothedDolphin\nSeaOtter\nShort_Finned(Pacific)PilotWhale\nSouthernRightWhale\nSpermWhale"
 
-  # Build the dataframe from the sound
+  # Build the dataframe from the sound 🗄
   dataframe = build_dataframe()
 
+  # encode the labels (0 => 44) 🔢
   df_labels = pandas.read_csv(io.StringIO(labels), header=None)
-
-  # encode the labels (0 => 44)
   converter = LabelEncoder()
   converter.fit_transform(df_labels.iloc[:,-1])
   
-  # INPUTS: all other columns are inputs except the filename
+  # Uniformize data thnaks to the initial data 🧹
   scaler = StandardScaler()
   df = pandas.read_csv(data_path + '/data.csv')
   scaler.fit(np.array(df.iloc[:, 1:27]))
   x = scaler.transform(np.array(dataframe.iloc[:, 1:27]))
 
-  # load the pretrained model
+  # load the pretrained model 🧠
   model = load_model(data_path + '/ai-model/model-marine-mammal-sounds-classification')
   
   # generate prediction
-  predictions = model(x)
+  predictions = model(x) 🧐
   
   # generate argmax for predictions
-  classes = np.argmax(predictions, axis = 1)
+  classes = np.argmax(predictions, axis = 1) 🔝
   
   # transform class number into class name
-  result = converter.inverse_transform(classes)
+  result = converter.inverse_transform(classes) 🐳
 
   return result
 
